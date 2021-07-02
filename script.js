7c51ab199073998f731f82d3a5a243effc560d40
Chandra-Kurnia <chandrakqurniawan2002@gmail.com>
2021-07-02 21:40:25 +0700

    try 2 ul

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 $('.dashboard').click(function(){
-    $('nav ul').toggleClass("item");
-    $('nav ul li a span').toggleClass("arrow-rotate");
+    $('nav .dash').toggleClass("item");
+    $('nav .dash li a span').toggleClass("arrow-rotate");
+});
+
+$('.profile').click(function(){
+    $('nav .profile').toggleClass("item");
+    $('nav .profile li a span').toggleClass("arrow-rotate");
 });
